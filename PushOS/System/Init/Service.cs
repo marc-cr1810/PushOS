af888fdedef4b7fa53182ede4ce0ee80547c2cd5
marc-cr1810 <marc.cookerussell@gmail.com>
2025-02-15 17:01:16 +1100

    Renamed

---
+++ PushOS/System/Init/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushOS.System.Init
+{
+    internal class Service
+    {
+        public Service(string file)
+        {
+
+        }
+    }
+}
